691b06cee2f1e5fa380cb28856c2ec98765272a8
Fraser May <frasertmay@gmail.com>
2017-06-24 11:15:32 +0100

    Modified how the clock is rendered to remove need for custom CSS

--- spec/atom-clock-view-spec.js
+++ spec/atom-clock-view-spec.js
@@ -61,7 +61,7 @@ describe('Atom Clock', () => {
   })
 
   it('should show the time with the default format', () => {
-    date = workspaceElement.querySelector('.atom-clock > span').innerText
+    date = workspaceElement.querySelector('.atom-clock').textContent
     expect(date).toBe('4:00')
   })
 
@@ -72,11 +72,11 @@ describe('Atom Clock', () => {
 
   it('should change the format of displayed time', () => {
     atom.config.set('atom-clock.dateFormat', 'H:mm:ss')
-    date = workspaceElement.querySelector('.atom-clock > span').innerText
+    date = workspaceElement.querySelector('.atom-clock').textContent
     expect(date).toBe('4:00:00')
 
     atom.config.set('atom-clock.dateFormat', 'DD dddd YY H:mm')
-    date = workspaceElement.querySelector('.atom-clock > span').innerText
+    date = workspaceElement.querySelector('.atom-clock').textContent
     expect(date.toLowerCase()).toBe('11 saturday 87 4:00')
   })
 
@@ -93,7 +93,7 @@ describe('Atom Clock', () => {
   it('should change the clock content according with the locale', () => {
     atom.config.set('atom-clock.dateFormat', 'DD dddd YY H:mm')
     atom.config.set('atom-clock.locale', 'it')
-    date = workspaceElement.querySelector('.atom-clock > span').innerText
+    date = workspaceElement.querySelector('.atom-clock').textContent
     expect(date.toLowerCase()).toBe('11 sabato 87 4:00')
   })
 
