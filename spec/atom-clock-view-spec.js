a8e0177df5937144fa8843d20c9ed0d5bfa568a1
Fraser May <frasertmay@gmail.com>
2017-06-25 11:08:12 +0100

    Added classes to icon and time elements and refactored specs accordingly

--- spec/atom-clock-view-spec.js
+++ spec/atom-clock-view-spec.js
@@ -2,6 +2,10 @@
 
 MockDate = require('mockdate')
 
+getDate = function(workspaceElement) {
+  return workspaceElement.querySelector('.atom-clock-time').textContent
+}
+
 getTooltips = function(workspaceElement) {
   element = workspaceElement.querySelector('.atom-clock')
   return atom.tooltips.findTooltips(element)
@@ -50,18 +54,18 @@ describe('Atom Clock', () => {
   })
 
   it('should not show the clock icon by default', () => {
-    icon = workspaceElement.querySelector('.atom-clock > .icon-clock')
+    icon = workspaceElement.querySelector('.atom-clock-icon.icon-clock')
     expect(icon).toBeNull()
   })
 
   it('should show the clock icon when configured', () => {
     atom.config.set('atom-clock.showClockIcon', true)
-    icon = workspaceElement.querySelector('.atom-clock > .icon-clock')
+    icon = workspaceElement.querySelector('.atom-clock-icon.icon-clock')
     expect(icon).not.toBeNull()
   })
 
   it('should show the time with the default format', () => {
-    date = workspaceElement.querySelector('.atom-clock').textContent
+    date = getDate(workspaceElement)
     expect(date).toBe('4:00')
   })
 
@@ -72,11 +76,11 @@ describe('Atom Clock', () => {
 
   it('should change the format of displayed time', () => {
     atom.config.set('atom-clock.dateFormat', 'H:mm:ss')
-    date = workspaceElement.querySelector('.atom-clock').textContent
+    date = getDate(workspaceElement)
     expect(date).toBe('4:00:00')
 
     atom.config.set('atom-clock.dateFormat', 'DD dddd YY H:mm')
-    date = workspaceElement.querySelector('.atom-clock').textContent
+    date = getDate(workspaceElement)
     expect(date.toLowerCase()).toBe('11 saturday 87 4:00')
   })
 
@@ -93,7 +97,7 @@ describe('Atom Clock', () => {
   it('should change the clock content according with the locale', () => {
     atom.config.set('atom-clock.dateFormat', 'DD dddd YY H:mm')
     atom.config.set('atom-clock.locale', 'it')
-    date = workspaceElement.querySelector('.atom-clock').textContent
+    date = getDate(workspaceElement)
     expect(date.toLowerCase()).toBe('11 sabato 87 4:00')
   })
 
